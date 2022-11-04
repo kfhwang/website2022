b2aea3bfa52b2efeb9ed06d8b29491f0ec344451
kfhwang <kfhwang@nutc.edu.tw>
2022-11-04 12:03:00 +0800

    20221104

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+server = express();
+
+server.use(express.static("Bootstrap_Exercise2"));//web root
+ 
+server.use(bodyParser.urlencoded());
+server.use(bodyParser.json());
+ 
+server.get("/portfolio", function(req, res){
+    portfolios= [
+        { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
+        { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
+        { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" },
+        { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
+        { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
+        { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" }
+    ]
+   res.send(portfolios);
+})
+  
+server.listen(8080, function(){
+    console.log("Server is running at port 8080!")
+})
